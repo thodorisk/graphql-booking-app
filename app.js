cae2211d1f5de0b7b2865a9b13d414f7e980fcfa
thodoris <thodorisk@icloud.com>
2019-12-21 15:09:18 +0200

    Implements a graphql API

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQLHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => res.send('Hello GraphQL'));
+app.use('/graphql', graphQLHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery,
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Sailing', 'All-Night Coding'];
+        },
+
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
 app.listen(3000);
