3bd3d665eddb88aab5279b81ab0dcacc9fa77042
thodoris <thodorisk@icloud.com>
2020-02-08 22:45:04 +0200

    Adds types

--- app.js
+++ app.js
@@ -4,16 +4,35 @@ const graphQLHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
+
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphQLHttp({
     schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,12 +42,20 @@ app.use('/graphql', graphQLHttp({
     `),
     rootValue: {
         events: () => {
-            return ['Sailing', 'All-Night Coding'];
+            return events;
         },
 
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            }
+
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
