f8e862ca315e9455b79806f3fba8e5fe8509c591
thodoris <thodorisk@icloud.com>
2020-02-09 13:42:42 +0200

    finalizes db connect to work/store real data

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const bodyParser = require("body-parser");
 const graphQLHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
+
+const Event = require("./models/event");
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -46,20 +46,35 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(e => {
+              return { ...e._doc, _id: e._doc._id.toString() };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
 
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
+          date: new Date(args.eventInput.date)
+        });
 
-        events.push(event);
-        return event;
+        return event
+          .save()
+          .then(res => {
+            console.log(res);
+            return { ...res._doc, _id: res._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
@@ -68,7 +83,7 @@ app.use(
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustergraphql-1fbal.mongodb.net/test?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustergraphql-1fbal.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
   )
   .then(() => {
