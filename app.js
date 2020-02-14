2a7a52c1560309e82ff2d36abc6caeaf7122728f
thodoris <thodorisk@icloud.com>
2020-02-14 21:02:05 +0200

    added relations between events and users

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const graphQLHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
@@ -24,11 +26,22 @@ app.use(
             date: String!
         }
 
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
         }
 
         type RootQuery {
@@ -37,6 +50,7 @@ app.use(
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -73,6 +87,27 @@ app.use(
           })
           .catch(err => {
             console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) throw new Error("A user with this email already exists.");
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPass => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPass
+            });
+
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
             throw err;
           });
       }
